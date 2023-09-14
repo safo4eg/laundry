2ce71f8d94cf0dfa20b7155e3c4a9ee9c7ed861e
momiamgood <forstuding411@gmail.com>
2023-09-15 01:17:55 +0700

    Added class Template, refactor InlinePageTrait

--- app/Http/Webhooks/Handlers/Traits/InlinePageTrait.php
+++ app/Http/Webhooks/Handlers/Traits/InlinePageTrait.php
@@ -2,94 +2,51 @@
 
 namespace App\Http\Webhooks\Handlers\Traits;
 
-use DefStudio\Telegraph\Keyboard\Button;
-use DefStudio\Telegraph\Keyboard\Keyboard;
+use App\Services\Template;
+use App\Models\User as UserModel;
 
 trait InlinePageTrait
 {
     public function send_select_language(): int
     {
-        $telegraph_response = $this->chat->message(view('bot.select_language'))
-            ->keyboard(Keyboard::make()->buttons([
-                Button::make('English')->action('select_language')->param('choice', 'en'),
-                Button::make('Русский')->action('select_language')->param('choice', 'ru')
-            ]))->send();
-        return $telegraph_response->telegraphMessageId();
-    }
-    public function send_inline_page(string $language_code, array $template_info, array $buttons_info): int
-    {
-        $buttons = $this->get_buttons($language_code, $buttons_info);
-        $keyboard = $this->create_keyboard($buttons);
-        $template_name = $template_info['template_name'];
-        $template_vars = isset($template_info['vars'])? $template_info['vars']: [];
-        $telegraph_response = $this->chat->message(view("bot.$language_code.$template_name", $template_vars))
-            ->keyboard($keyboard)
-            ->send();
-        return $telegraph_response->telegraphMessageId();
+        return $this->sendMessage('select_language');
     }
 
-    public function next_inline_page(int $message_id, string $language_code, array $template_info, array $buttons_info):void
+    public function sendMessage(string $template, array $templateData = []): ?int
     {
-        $template_name = $template_info['template_name'];
-        $params = isset($template_info['params'])? $template_info['params']: [];
+        $template = $this->createTemplate($template, $templateData);
 
-        $buttons = $this->get_buttons($language_code, $buttons_info);
-        $keyboard = $this->create_keyboard($buttons);
-        $this->chat->edit($message_id)
-            ->message(view("bot.$language_code.$template_name", $params))
-            ->keyboard($keyboard)
-            ->send();
-    }
-
-    private function create_keyboard(array $buttons): Keyboard
-    {
-        $keyboard = Keyboard::make();
-        foreach ($buttons as $elem) {
-            if($elem instanceof Button) {
-                $keyboard->buttons($buttons);
-                return $keyboard;
-            }
-            $keyboard->row($elem);
-        }
-        return $keyboard;
-    }
-
-    private function get_buttons(string $language_code, array $buttons_info): array
-    {
-        $buttons = [];
-
-        if(array_key_exists('row', $buttons_info)) {
-            $type = 'row';
-            foreach ($buttons_info as $row_info) {
-                $row = [];
-                foreach ($row_info as $button_info) {
-                    $button = $this->get_button($button_info, $language_code);
-                    $row[] = $button;
-                }
-                $buttons[] = $row;
-            }
+        if (!$template->keyboard == null) {
+            $telegraph_response = $this->chat->message($template->template)
+                ->keyboard($template->keyboard)
+                ->send();
         } else {
-            foreach ($buttons_info as $button_info) {
-                $button = $this->get_button($button_info, $language_code);
-                $buttons[] = $button;
-            }
+            $telegraph_response = $this->chat->message($template->template)
+                ->send();
         }
 
-        return $buttons;
+        return $telegraph_response->telegraphMessageId();
     }
 
-    private function get_button(array $button_info, string $language_code): Button
+    public function editMessage(int $message_id, string $template, array $template_data): void
     {
-        $button = null;
-        $button = Button::make($button_info['language'][$language_code]);
-        switch ($button_info['type']) {
-            case 'action':
-                $button->action($button_info['method']);
-                $button->param($button_info['param'][0], $button_info['param'][1]);
-                break;
-            case 'url':
-                $button->url($button_info['link']);
+        $template = $this->createTemplate($template, $template_data);
+
+        if (!$template->keyboard == null) {
+            $this->chat->edit($message_id)
+                ->message($template->template)
+                ->keyboard($template->keyboard)
+                ->send();
+        } else {
+            $this->chat->edit($message_id)
+                ->message($template->template)
+                ->send();
         }
-        return $button;
+    }
+
+    private function createTemplate(string $template, array $templateData): Template
+    {
+        $user = UserModel::where('chat_id', $this->message->from()->id())->first();
+        return new Template($template, $user->language_code, $templateData);
     }
 }
