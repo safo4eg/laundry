81e6147f4a594a5ae52c3e982280c3d699c6b07f
momiamgood <forstuding411@gmail.com>
2023-10-01 20:19:56 +0700

    Add new buttons

--- config/buttons.php
+++ config/buttons.php
@@ -1,82 +1,83 @@
 <?php
-    return [
-        'user' => [
-            'start' => [
-                'start' => ['ru' => 'Начать', 'en' => 'Start'],
-                'reviews' => ['ru' => 'Отзывы', 'en' => 'Reviews']
-            ],
-
-            'select_language' => [
-                'russia' => 'Русский',
-                'english' => 'English',
-                'back' => ['ru' => 'Вернуться назад', 'en' => 'Go to back']
-            ],
-
-            'send_location' => ['ru' => 'Отправить локацию', 'en' => 'Send location'],
-            'send_contact' => ['ru' => 'Отправить номер', 'en' => 'Send number'],
-            'accept_order' => ['ru' => 'Заказать стирку', 'en' => 'Order Laundry'],
-
-            'order_accepted' => [
-                'wishes' => ['ru' => 'Особые пожелания по стирке', 'en' => 'Laundry Special Requests'],
-                'cancel' => ['ru' => 'Отменить заказ', 'en' => 'Cancel order'],
-                'recommend' => ['ru' => 'Рекомендовать друзьям', 'en' => 'Recommend to friends']
-            ],
-
-            'order_wishes' => ['ru' => 'Назад', 'en' => 'Back'],
-
-            'cancel_order' => [
-                'check_bot' => ['ru' => 'Просто решил проверить Бот', 'en' => 'Just decided to check the Bot'],
-                'changed_my_mind' => ['ru' => 'Передумал стирать', 'en' => 'Changed my mind'],
-                'quality' => ['ru' => 'Переживаю за качество стирки', 'en' => 'Worried about the quality of the wash'],
-                'expensive' => ['ru' => 'Дорого', 'en' => 'Expensive'],
-                'back' => ['ru' => '⬅️ Вернуться в заказ', 'en' => '⬅️ Back to order']
-            ],
-
-            'order_canceled' => [
-                'start' => ['ru' => 'Заказать стирку', 'en' => 'Order Laundry'],
-                'recommend' => ['ru' => 'Рекомендовать друзьям', 'en' => 'Recommend to friends']
-            ],
-
-            'about_us' => ['ru' => 'Начать', 'en' => 'Start'],
-            'orders' => [
-                'order_laundry' => ['ru' => 'Заказать стирку', 'en' => 'Order laundry'],
-                'active' => ['ru' => 'Активные', 'en' => 'Active'],
-                'canceled' => ['ru' => 'Отменённые', 'en' => 'Canceled'],
-                'completed' => ['ru' => 'Завершенные', 'en' => 'Completed'],
-                'back' => ['ru' => 'Вернуться назад', 'en' => 'Go to back'],
-            ],
-
-            'order_info' => [
-                'wishes' => ['ru' => 'Особые пожелания по стирке', 'en' => 'Laundry Special Requests'],
-                'cancel' => ['ru' => 'Отменить заказ', 'en' => 'Cancel order'],
-                'recommend' => ['ru' => 'Рекомендовать друзьям', 'en' => 'Recommend to friends'],
-                'back' => ['ru' => 'Вернуться назад', 'en' => 'Go to back'],
-            ],
-
-            'profile' => [
-                'phone_number' => ['ru' => 'Изменить номер телефона', 'en' => 'Change phone number'],
-                'whatsapp' => ['ru' => 'Изменить whatsapp', 'en' => 'Change whatsapp'],
-                'language' => ['ru' => 'Сменить язык', 'en' => 'Change language'],
-                'back' => ['ru' => 'Вернуться назад', 'en' => 'Go to back']
-            ],
-
-            'referrals' => [
-                'recommend' => ['ru' => 'Рекомендовать другу', 'en' => 'Recommend to friends'],
-                'start' => ['ru' => 'Заказать стирку', 'en' => 'Order Laundry'],
-                'info' => ['ru' => 'Мои рефералы', 'en' => 'My referrals'],
-                'back' => ['ru' => 'Вернуться назад', 'en' => 'Go to back']
-            ],
-            // обернуть begin
-            'support' => ['ru' => 'Написать', 'en' => 'Write'],
-            'confirm' => [
-                'yes' => ['ru' => 'Да', 'en' => 'Yes'],
-                'no' => ['ru' => 'Нет', 'en' => 'No']
-            ],
-            'lc' => [
-                'active' => ['ru' => 'Активные', 'en' => 'Active'],
-                'archive' => ['ru' => 'Архивные', 'en' => 'Archive']
-            ],
-            'back' => ['ru' => 'Назад', 'en' => 'Back']
-            // end
+return [
+    'user' => [
+        'start' => [
+            'start' => ['ru' => 'Начать', 'en' => 'Start'],
+            'reviews' => ['ru' => 'Отзывы', 'en' => 'Reviews']
         ],
-    ];
+
+        'select_language' => [
+            'russia' => 'Русский',
+            'english' => 'English',
+            'back' => ['ru' => 'Вернуться назад', 'en' => 'Go to back']
+        ],
+
+        'send_location' => ['ru' => 'Отправить локацию', 'en' => 'Send location'],
+        'send_contact' => ['ru' => 'Отправить номер', 'en' => 'Send number'],
+        'accept_order' => ['ru' => 'Заказать стирку', 'en' => 'Order Laundry'],
+
+        'order_accepted' => [
+            'wishes' => ['ru' => 'Особые пожелания по стирке', 'en' => 'Laundry Special Requests'],
+            'cancel' => ['ru' => 'Отменить заказ', 'en' => 'Cancel order'],
+            'recommend' => ['ru' => 'Рекомендовать друзьям', 'en' => 'Recommend to friends']
+        ],
+
+        'order_wishes' => ['ru' => 'Назад', 'en' => 'Back'],
+
+        'cancel_order' => [
+            'check_bot' => ['ru' => 'Просто решил проверить Бот', 'en' => 'Just decided to check the Bot'],
+            'changed_my_mind' => ['ru' => 'Передумал стирать', 'en' => 'Changed my mind'],
+            'quality' => ['ru' => 'Переживаю за качество стирки', 'en' => 'Worried about the quality of the wash'],
+            'expensive' => ['ru' => 'Дорого', 'en' => 'Expensive'],
+            'back' => ['ru' => '⬅️ Вернуться в заказ', 'en' => '⬅️ Back to order']
+        ],
+
+        'order_canceled' => [
+            'start' => ['ru' => 'Заказать стирку', 'en' => 'Order Laundry'],
+            'recommend' => ['ru' => 'Рекомендовать друзьям', 'en' => 'Recommend to friends']
+        ],
+
+        'about_us' => ['ru' => 'Начать', 'en' => 'Start'],
+        'orders' => [
+            'order_laundry' => ['ru' => 'Заказать стирку', 'en' => 'Order laundry'],
+            'active' => ['ru' => 'Активные', 'en' => 'Active'],
+            'canceled' => ['ru' => 'Отменённые', 'en' => 'Canceled'],
+            'completed' => ['ru' => 'Завершенные', 'en' => 'Completed'],
+            'back' => ['ru' => 'Вернуться назад', 'en' => 'Go to back'],
+        ],
+
+        'order_info' => [
+            'wishes' => ['ru' => 'Особые пожелания по стирке', 'en' => 'Laundry Special Requests'],
+            'cancel' => ['ru' => 'Отменить заказ', 'en' => 'Cancel order'],
+            'recommend' => ['ru' => 'Рекомендовать друзьям', 'en' => 'Recommend to friends'],
+            'back' => ['ru' => 'Вернуться назад', 'en' => 'Go to back'],
+        ],
+
+        'profile' => [
+            'phone_number' => ['ru' => 'Изменить номер телефона', 'en' => 'Change phone number'],
+            'whatsapp' => ['ru' => 'Изменить whatsapp', 'en' => 'Change whatsapp'],
+            'language' => ['ru' => 'Сменить язык', 'en' => 'Change language'],
+            'back' => ['ru' => 'Вернуться назад', 'en' => 'Go to back']
+        ],
+
+        'referrals' => [
+            'recommend' => ['ru' => 'Рекомендовать другу', 'en' => 'Recommend to friends'],
+            'start' => ['ru' => 'Заказать стирку', 'en' => 'Order Laundry'],
+            'info' => ['ru' => 'Мои рефералы', 'en' => 'My referrals'],
+            'back' => ['ru' => 'Вернуться назад', 'en' => 'Go to back']
+        ],
+        // обернуть begin
+        'support' => ['ru' => 'Написать', 'en' => 'Write'],
+        'confirm' => [
+            'yes' => ['ru' => 'Да', 'en' => 'Yes'],
+            'no' => ['ru' => 'Нет', 'en' => 'No']
+        ],
+        'lc' => [
+            'active' => ['ru' => 'Активные', 'en' => 'Active'],
+            'archive' => ['ru' => 'Архивные', 'en' => 'Archive']
+        ],
+        'back' => ['ru' => 'Назад', 'en' => 'Back'],
+        'tickets' => ['ru' => 'Проверить статус заявки', 'en' => 'Check application status' ]
+        // end
+    ],
+];
