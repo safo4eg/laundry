8c377f4b0129e19baee2b05ffed7b54e4bf57f05
safo4eg <stas.rakov111@mail.ru>
2023-10-26 19:47:39 +0300

    Реализована отправка фейк-запроса(тригер обработчика юзера) после написания курьером сообдения в ордер_месседж

--- app/Observers/OrderMessageObserver.php
+++ app/Observers/OrderMessageObserver.php
@@ -2,8 +2,13 @@
 
 namespace App\Observers;
 
+use App\Http\Webhooks\Handlers\User;
+use App\Models\Bot;
+use App\Models\Chat;
 use App\Models\OrderMessage;
+use App\Services\FakeRequest;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class OrderMessageObserver
 {
@@ -20,8 +25,28 @@ class OrderMessageObserver
 
     public function created(OrderMessage $orderMessage)
     {
+        $bot = Bot::where('username', 'rastan_telegraph_bot')->first();
         $order = $orderMessage->order;
-        /* Отправка сообщения в чат курьеру */
+        /* если в таблице чатов есть такой чат => написал курьер, иначе юзер */
+        if(Chat::where('chat_id', $orderMessage->sender_chat_id)->first()) { // написал курьер => отправка юзеру
+            $chat = Chat::factory()->make([
+                'chat_id' => $order->user->chat_id,
+                'name' => 'User',
+                'telegraph_bot_id' => 1
+            ]);
+
+            $fake_dataset = [
+                'action' => 'order_dialogue',
+                'params' => [
+                    'dialogue' => 1,
+                    'get' => 1,
+                    'order_id' => $order->id
+                ]
+            ];
+
+            $fake_request = FakeRequest::callback_query($chat, $bot, $fake_dataset);
+            (new User($order->user))->handle($fake_request, $bot);
+        }
 
     }
 
