fb768cb8cbc471ed78aeb956261b252859669e8c
safo4eg <stas.rakov111@mail.ru>
2023-10-29 01:00:16 +0300

    Изменена логика отправки на отображения карточки заказа с просьбой сфотать деньги

--- app/Observers/PaymentObserver.php
+++ app/Observers/PaymentObserver.php
@@ -33,28 +33,20 @@ class PaymentObserver
 
             $fake_request = FakeRequest::callback_query($courier_chat, $bot, $fake_callback_dataset);
 
-            /* Если курьер ожидает оплаты наличными */
-            if($order->status_id === 13) {
-                /* Если сменили на оплату картой */
+            $main_chat_order = ChatOrderPivot::where('telegraph_chat_id', $courier_chat->id)
+                ->where('order_id', $order->id)
+                ->where('message_type_id', 1)
+                ->first();
+
+            /* Если статус_ид = 13 и есть карточка => ожидает оплату наличными */
+            if($order->status_id === 13 AND isset($main_chat_order)) {
                 if(in_array($attributes['method_id'], [2, 3])) {
-                    $order->status_id = 14;
-                    $order->saveQuietly();
-
-                    $main_chat_order = ChatOrderPivot::where('telegraph_chat_id', $courier_chat->id)
-                        ->where('order_id', $order->id)
-                        ->where('message_type_id', 1)
-                        ->first();
-
-                    if(isset($main_chat_order)) {
-                        /* отправляем на обновление карточки заказа */
-                        /* со статусом заказа = 14 не зайдет в отправку новой карточки */
-                        (new Courier())->handle($fake_request, $bot);
-                    }
+                    /* отправляем на обновление карточки заказа */
+                    /* со статусом заказа = 13 без метода оплаты=1 не зайдет в отправку новой карточки */
+                    (new Courier())->handle($fake_request, $bot);
                 }
-            } else if($order->status_id === 14) {
+            } else if($order->status_id === 13 AND !isset($main_chat_order)) {
                 if($attributes['method_id'] == 1) {
-                    $order->status_id = 13;
-                    $order->saveQuietly();
                     (new Courier())->handle($fake_request, $bot);
                 }
             }
