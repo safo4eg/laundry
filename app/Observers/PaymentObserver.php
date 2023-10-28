099834089d231d35cff4ee57255e362a060eff89
safo4eg <stas.rakov111@mail.ru>
2023-10-28 20:46:46 +0300

    Добавлена отправка карточки заказа курьеру, если после доставки был изменён способ оплаты

--- app/Observers/PaymentObserver.php
+++ app/Observers/PaymentObserver.php
@@ -19,15 +19,26 @@ class PaymentObserver
         $attributes = $payment->getDirty();
 
         if(isset($attributes['method_id'])) {
+            $courier_chat = Chat::where('name', 'Courier')
+                ->where('laundry_id', $order->laundry_id)
+                ->first();
+
+            $fake_callback_dataset = [
+                'action' => 'update_order_card',
+                'params' => [
+                    'update_order_card' => 1,
+                    'order_id' => $order->id
+                ]
+            ];
+
+            $fake_request = FakeRequest::callback_query($courier_chat, $bot, $fake_callback_dataset);
+
             /* Если курьер ожидает оплаты наличными */
             if($order->status_id === 13) {
                 /* Если сменили на оплату картой */
                 if(in_array($attributes['method_id'], [2, 3])) {
-                    $order->update(['status_id' => 14]);
-
-                    $courier_chat = Chat::where('name', 'Courier')
-                        ->where('laundry_id', $order->laundry_id)
-                        ->first();
+                    $order->status_id = 14;
+                    $order->saveQuietly();
 
                     $main_chat_order = ChatOrderPivot::where('telegraph_chat_id', $courier_chat->id)
                         ->where('order_id', $order->id)
@@ -35,18 +46,16 @@ class PaymentObserver
                         ->first();
 
                     if(isset($main_chat_order)) {
-                        /* отправляем на удаление карточки заказа */
-                        $fake_callback_dataset = [
-                            'action' => 'update_order_card',
-                            'params' => [
-                                'update_order_card' => 1,
-                                'order_id' => $order->id
-                            ]
-                        ];
-
-                        $fake_request = FakeRequest::callback_query($courier_chat, $bot, $fake_callback_dataset);
+                        /* отправляем на обновление карточки заказа */
+                        /* со статусом заказа = 14 не зайдет в отправку новой карточки */
                         (new Courier())->handle($fake_request, $bot);
                     }
+                }
+            } else if($order->status_id === 14) {
+                if($attributes['method_id'] == 1) {
+                    $order->status_id = 13;
+                    $order->saveQuietly();
+                    (new Courier())->handle($fake_request, $bot);
                 }
             }
         }
