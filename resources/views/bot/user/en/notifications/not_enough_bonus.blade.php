db944d5d0a33c67e72f2594f83574e9dcd9f876b
safo4eg <stas.rakov111@mail.ru>
2023-11-01 19:27:42 +0300

    Добавлены шаблоны en/ru 'недостаточно бонусов для оплаты'

---
+++ resources/views/bot/user/en/notifications/not_enough_bonus.blade.php
@@ -0,0 +1,4 @@
+😢 There are not enough bonuses on your balance!
+
+In order to get bonuses, share our bot with your friends,
+EVERY TIME your friends wash their clothes with us, you will receive 10% cashback on their order amount.
