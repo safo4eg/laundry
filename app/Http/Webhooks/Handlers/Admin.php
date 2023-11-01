2eef55299c89bc3f6f4074e0ddfd07e03d515b87
safo4eg <stas.rakov111@mail.ru>
2023-11-01 15:15:29 +0300

    Реализован функционал кнопки подтвердить: Admin

--- app/Http/Webhooks/Handlers/Admin.php
+++ app/Http/Webhooks/Handlers/Admin.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Webhooks\Handlers;
 
+use App\Http\Webhooks\Handlers\Traits\ChatsHelperTrait;
+use App\Models\Chat;
 use App\Models\ChatOrderPivot;
 use App\Models\File;
+use App\Services\FakeRequest;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 use App\Models\Order;
 use DefStudio\Telegraph\Keyboard\Button;
@@ -12,6 +15,7 @@ use Illuminate\Support\Facades\Storage;
 
 class Admin extends WebhookHandler
 {
+    use ChatsHelperTrait;
     public function __construct()
     {
         $this->buttons = config('buttons.admin');
@@ -33,16 +37,50 @@ class Admin extends WebhookHandler
                 $choice = $this->data->get('choice');
 
                 if ($choice === 'yes') {
+                    $tempt_file = File::where('order_id', $order->id)
+                        ->where('file_type_id', 1)
+                        ->where('order_status_id', null)
+                        ->first();
+                    $photo_path = $tempt_file->path;
+                    $tempt_file->delete();
 
+                    File::create([
+                        'order_id' => $order->id,
+                        'ticket_item_id' => null,
+                        'file_type_id' => 1,
+                        'order_status_id' => 14,
+                        'path' => $photo_path
+                    ]);
+
+                    $order->payment->update(['status_id' => 3]);
+                    $order->update(['status_id' => 14]);
+
+                    $fake_dataset = [
+                        'action' => 'send_card',
+                        'params' => [
+                            'order_id' => $order->id,
+                            'edit' => 1
+                        ]
+                    ];
+
+                    $fake_request = FakeRequest::callback_query($this->chat, $this->bot, $fake_dataset);
+                    (new Admin())->handle($fake_request, $this->bot);
                 }
 
                 if ($choice === 'no') {
+                    $this->delete_order_card_messages($order, true);
+                    $order->payment->update([
+                        'method_id' => null,
+                        'status_id' => 1
+                    ]);
                 }
 
             }
         }
 
         if (!isset($flag)) {
+            $edit = $this->data->get('edit');
+
             $template = $this->template_prefix . 'order_info';
             $buttons_texts = $this->buttons['send_card'];
             $buttons = [];
@@ -82,18 +120,33 @@ class Admin extends WebhookHandler
             }
 
             $keyboard = Keyboard::make()->buttons($buttons);
-            $response = $this->chat
-                ->photo(Storage::path($photo_path))
-                ->html(view($template, ['order' => $order]))
-                ->keyboard($keyboard)
-                ->send();
 
-            ChatOrderPivot::create([
-                'telegraph_chat_id' => $this->chat->id,
-                'order_id' => $order->id,
-                'message_id' => $response->telegraphMessageId(),
-                'message_type_id' => 1
-            ]);
+            if(isset($edit)) {
+                $chat_order = ChatOrderPivot::where('telegraph_chat_id', $this->chat->id)
+                    ->where('order_id', $order->id)
+                    ->where('message_type_id', 1)
+                    ->first();
+
+                $this->chat
+                    ->editMedia($chat_order->message_id)
+                    ->photo(Storage::path($photo_path))
+                    ->html(view($template, ['order' => $order]))
+                    ->keyboard($keyboard)
+                    ->send();
+            } {
+                $response = $this->chat
+                    ->photo(Storage::path($photo_path))
+                    ->html(view($template, ['order' => $order]))
+                    ->keyboard($keyboard)
+                    ->send();
+
+                ChatOrderPivot::create([
+                    'telegraph_chat_id' => $this->chat->id,
+                    'order_id' => $order->id,
+                    'message_id' => $response->telegraphMessageId(),
+                    'message_type_id' => 1
+                ]);
+            }
         }
 
     }
