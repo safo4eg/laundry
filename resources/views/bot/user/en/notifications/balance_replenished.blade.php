dff59a7c0d21c9456a86524c9f7a6622f2f08c2f
safo4eg <stas.rakov111@mail.ru>
2023-11-02 12:15:40 +0300

    Добавлены шаблоны en/ru пополнения баланса пользователя

---
+++ resources/views/bot/user/en/notifications/balance_replenished.blade.php
@@ -0,0 +1,6 @@
+🎉Your balance has been replenished by <b>{{$plus}}</b> IDR!
+
+<b>Now your balance is {{$user->balance}} IDR</b>
+
+☝️ With the help of bonuses you can pay for your laundry,
+both partially and completely!
