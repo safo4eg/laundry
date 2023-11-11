48bec876d07fe42f36bdd0da71d7a0986b9e1789
momiamgood <forstuding411@gmail.com>
2023-11-11 16:22:25 +0700

    Фикс карточки заказа

--- resources/views/bot/chats/order_info.blade.php
+++ resources/views/bot/chats/order_info.blade.php
@@ -1,15 +1,17 @@
 <b>Order #{{$order->id}}</b>
-@if($order->status_id === 2)
-    Status: <b>Ready to be sent to the courier</b>
-@elseif($order->status_id === 3)
-    Status: <b>Sent to courier</b>
-@endif
-Wishes: {{isset($order->wishes)? $order->wishes: 'NULL'}}
+<b>Wishes:</b> {{isset($order->wishes)? $order->wishes: 'NULL'}}
 
 User id: <b>{{$order->user->id}}</b>
 User login: <b>{{"@".$order->user->username}}</b>
 User orders count: <b>{{$order->user->orders()->count()}}</b>
 Number: <b>{{$order->user->phone_number}}</b>
 Whatsapp: <b>{{($order->user->whatsapp)? $order->user->whatsapp: 'Не указан'}}</b>
-Location: <b>{{$order->address}}</b>
+Location: <b><a href="https://www.google.com/maps/place/{{$order->geo}}">{{$order->address}}</a></b>
 Place: <b>{{$order->address_desc}}</b>
+
+@foreach(($order->statuses) as $status)
+    {{ $status->en_desc }}: {{ $status->pivot->created_at }}
+@endforeach
+
+
+
